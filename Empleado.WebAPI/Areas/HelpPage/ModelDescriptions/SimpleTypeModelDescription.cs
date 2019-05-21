bd0f54878554736dbf2198b452a7dd5cc3b20ea0
Miguel319 <miguelp90.319@gmail.com>
2019-05-21 16:00:35 -0400

    Add project files.

---
+++ Empleado.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Empleado.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Empleado.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Empleado.WebAPI.WebApiApplication" Language="C#" %>
